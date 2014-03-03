4dfd063a151907fad53ce9b6d8d2364a446dba26
Brian Hines <brian@projectweekend.net>
2014-03-03 08:13:17 -0600

    This isn't running right now...
    …but i’m changing it to match new schema anyway

--- workers/system_temp.py
+++ workers/system_temp.py
@@ -14,6 +14,7 @@ def get_system_temp():
     fahrenheit_temp = utils.celsius_to_fahrenheit(celsius_temp)
     return {
         'date': datetime.datetime.utcnow(),
+        'from': 'Holly',
         'celsius':celsius_temp, 
         'fahrenheit': fahrenheit_temp
     }
