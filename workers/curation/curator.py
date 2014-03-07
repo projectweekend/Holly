a50e4fd1decd1519b66a7cbad70f387a51cdffd3
Brian Hines <brian@projectweekend.net>
2014-03-07 09:32:49 -0600

    Cleaning up more stuff

--- workers/curation/curator.py
+++ workers/curation/curator.py
@@ -1,4 +1,3 @@
-import utils
 import database
 
 
--- workers/utils/__init__.py
+++ workers/utils/__init__.py
@@ -1,5 +1,4 @@
 from conversion_helpers import *
-from network_helpers import *
 from newspaper_helpers import *
 from parsers import *
 from twitter_helpers import get_twitter_client
