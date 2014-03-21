9c5a436b0902358607190bfc39afa64a5579c3f8
Brian Hines <brian@projectweekend.net>
2014-03-21 06:13:44 -0500

    Pull Forecast.io API key from my database config model

--- webapp/routes/weatherAPI.js
+++ webapp/routes/weatherAPI.js
@@ -1,14 +1,34 @@
-var weatherman = require('weatherman');
+var appModels = require( '../models' ),
+	weatherman = require('weatherman.io');
 
+ 
 var errorHandler = function ( err, res ) {
     console.log( err );
     res.send( 500 );
 };
 
 
+var forecastIOKey = "Not defined";
+var loadThirdPartyConfig = function () {
+
+	var q = ThirdPartyConfig.findOne({config_key: 'FORECAST_IO_API_KEY'});
+	q.exec( function ( err, data ) {
+		if ( data ) {
+			forecastIOKey = data.config_value;
+		}
+	} );
+
+};
+loadThirdPartyConfig();
+
+
 exports.currentWeather = function ( req, res ) {
-	// var weatherman = weatherman('test');
-	var theWeatherman = weatherman('test');
-	// console.log(w.apiKey);
-	res.json(theWeatherman.apiKey);
+	var theWeatherman = weatherman(forecastIOKey);
+	theWeatherman.goOnLocation(41.8854710, -87.6430260);
+	theWeatherman.doForecast( function ( err, weatherReport ) {
+		if ( err ) {
+			return errorHandler( err, res );
+		}
+		return res.json( weatherReport );
+	} );
 };
