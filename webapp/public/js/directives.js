27b42e1021f833799d3a4c735b72ddfa9c928e0c
Brian Hines <brian@projectweekend.net>
2014-03-23 17:16:09 -0500

    Cleaning up default directives

--- webapp/public/js/directives.js
+++ webapp/public/js/directives.js
@@ -1,10 +1,9 @@
 'use strict';
 
-/* Directives */
+var dMod = angular.module( 'myApp.directives', [] );
 
-angular.module('myApp.directives', []).
-  directive('appVersion', function (version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  });
+dMod.directive( 'appVersion', function ( version ) {
+	return function( scope, elm, attrs ) {
+		elm.text( version );
+	};
+} );
