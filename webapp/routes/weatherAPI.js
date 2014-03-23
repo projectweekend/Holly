27b6d25fdcd598efed1e435a4e6690bb53b0acf4
Brian Hines <brian@projectweekend.net>
2014-03-23 10:25:06 -0500

    Customize the output of the current weather route a bit

--- webapp/routes/weatherAPI.js
+++ webapp/routes/weatherAPI.js
@@ -8,6 +8,11 @@ var errorHandler = function ( err, res ) {
 };
 
 
+var convertDate = function ( unixTime ) {
+	return new Date( unixTime * 1000 );
+};
+
+
 var forecastIOKey = "Not defined";
 var loadThirdPartyConfig = function () {
 
@@ -23,14 +28,56 @@ loadThirdPartyConfig();
 
 
 exports.forecastIOCurrent = function ( req, res ) {
+
+	var latitude = req.query.latitude;
+	var longitude = req.query.longitude;
+
 	var theWeatherman = weatherman(forecastIOKey);
 	theWeatherman.options = {'exclude': ["minutely", "hourly", "daily", "flags"]};
-	theWeatherman.goOnLocation(41.8854710, -87.6430260);
+	theWeatherman.goOnLocation(latitude, longitude);
 	theWeatherman.doForecast( function ( err, weatherReport ) {
 		if ( err ) {
 			return errorHandler( err, res );
 		}
-		return res.json( weatherReport );
+		var output = {
+			dateTime: convertDate(weatherReport.currently.time),
+			icon: weatherReport.currently.icon,
+			summary: weatherReport.currently.summary,
+			temperature: weatherReport.currently.temperature,
+			feelsLike: weatherReport.currently.apparentTemperature,
+			windSpeed: weatherReport.currently.windSpeed,
+			windBearing: "N/A",
+			humidity: weatherReport.currently.humidity
+		};
+		if ( output.windSpeed > 0 ) {
+			var windBearing = weatherReport.currently.windBearing;
+			if ( windBearing === 0  ) {
+				output.windBearing = "North";
+			}
+			if ( windBearing < 90 ) {
+				output.windBearing = "North East";
+			}
+			if ( windBearing === 90 ) {
+				output.windBearing = "East";
+			}
+			if ( windBearing < 180 ) {
+				output.windBearing = "South East";
+			}
+			if ( windBearing === 180 ) {
+				output.windBearing = "South";
+			}
+			if ( windBearing < 270 ) {
+				output.windBearing = "South West";
+			}
+			if ( windBearing === 270 ) {
+				output.windBearing = "West";
+			}
+			if ( windBearing > 270 ) {
+				output.windBearing = "North West";
+			}
+		}
+
+		return res.json( output );
 	} );
 };
 
