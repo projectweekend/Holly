b503776cb0253c8c821a3d1ed03296835d31094e
Brian Hines <brian@projectweekend.net>
2014-03-23 10:50:32 -0500

    Extract function to parse a weather block and improve hourly route

--- webapp/routes/weatherAPI.js
+++ webapp/routes/weatherAPI.js
@@ -12,6 +12,51 @@ var convertDate = function ( unixTime ) {
 	return new Date( unixTime * 1000 );
 };
 
+var parseWeatherDataBlock = function ( weatherDataBlock ) {
+
+	var output = {
+		dateTime: convertDate(weatherDataBlock.time),
+		icon: weatherDataBlock.icon,
+		summary: weatherDataBlock.summary,
+		temperature: weatherDataBlock.temperature,
+		feelsLike: weatherDataBlock.apparentTemperature,
+		windSpeed: weatherDataBlock.windSpeed,
+		windBearing: "N/A",
+		humidity: weatherDataBlock.humidity
+	};
+
+	if ( output.windSpeed > 0 ) {
+		var windBearing = weatherDataBlock.windBearing;
+		if ( windBearing === 0  ) {
+			output.windBearing = "North";
+		}
+		if ( windBearing < 90 ) {
+			output.windBearing = "North East";
+		}
+		if ( windBearing === 90 ) {
+			output.windBearing = "East";
+		}
+		if ( windBearing < 180 ) {
+			output.windBearing = "South East";
+		}
+		if ( windBearing === 180 ) {
+			output.windBearing = "South";
+		}
+		if ( windBearing < 270 ) {
+			output.windBearing = "South West";
+		}
+		if ( windBearing === 270 ) {
+			output.windBearing = "West";
+		}
+		if ( windBearing > 270 ) {
+			output.windBearing = "North West";
+		}
+	}
+
+	return output;
+
+};
+
 
 var forecastIOKey = "Not defined";
 var loadThirdPartyConfig = function () {
@@ -34,67 +79,39 @@ exports.forecastIOCurrent = function ( req, res ) {
 
 	var theWeatherman = weatherman(forecastIOKey);
 	theWeatherman.options = {'exclude': ["minutely", "hourly", "daily", "flags"]};
-	theWeatherman.goOnLocation(latitude, longitude);
+	theWeatherman.goOnLocation( latitude, longitude );
 	theWeatherman.doForecast( function ( err, weatherReport ) {
 		
 		if ( err ) {
 			return errorHandler( err, res );
 		}
-
-		var output = {
-			dateTime: convertDate(weatherReport.currently.time),
-			icon: weatherReport.currently.icon,
-			summary: weatherReport.currently.summary,
-			temperature: weatherReport.currently.temperature,
-			feelsLike: weatherReport.currently.apparentTemperature,
-			windSpeed: weatherReport.currently.windSpeed,
-			windBearing: "N/A",
-			humidity: weatherReport.currently.humidity
-		};
-
-		if ( output.windSpeed > 0 ) {
-			var windBearing = weatherReport.currently.windBearing;
-			if ( windBearing === 0  ) {
-				output.windBearing = "North";
-			}
-			if ( windBearing < 90 ) {
-				output.windBearing = "North East";
-			}
-			if ( windBearing === 90 ) {
-				output.windBearing = "East";
-			}
-			if ( windBearing < 180 ) {
-				output.windBearing = "South East";
-			}
-			if ( windBearing === 180 ) {
-				output.windBearing = "South";
-			}
-			if ( windBearing < 270 ) {
-				output.windBearing = "South West";
-			}
-			if ( windBearing === 270 ) {
-				output.windBearing = "West";
-			}
-			if ( windBearing > 270 ) {
-				output.windBearing = "North West";
-			}
-		}
 		
-		return res.json( output );
+		return res.json( parseWeatherDataBlock(weatherReport.currently) );
 
 	} );
 };
 
 
 exports.forecastIOHourly = function ( req, res ) {
+
+	var latitude = req.query.latitude;
+	var longitude = req.query.longitude;
+
 	var theWeatherman = weatherman(forecastIOKey);
 	theWeatherman.options = {'exclude': ["minutely", "currently", "daily", "flags", "alerts"]};
-	theWeatherman.goOnLocation(41.8854710, -87.6430260);
+	theWeatherman.goOnLocation( latitude, longitude );
 	theWeatherman.doForecast( function ( err, weatherReport ) {
 		if ( err ) {
 			return errorHandler( err, res );
 		}
-		return res.json( weatherReport );
+
+		var output = [];
+
+		weatherReport.hourly.data.forEach( function ( element, index, array ) {
+			output.push( parseWeatherDataBlock( element ) );
+		} );
+
+		return res.json( output );
 	} );
 };
 
