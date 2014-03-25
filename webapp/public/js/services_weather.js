6c29ece2a85f60caf41363b5c22b8c5aad057207
Brian Hines <brian@projectweekend.net>
2014-03-25 07:49:07 -0500

    Trying out a status object on service

--- webapp/public/js/services_weather.js
+++ webapp/public/js/services_weather.js
@@ -14,15 +14,24 @@ svcMod.factory( "CurrentWeather", function ( $http ) {
 
     return {
         forecast: {},
+        status: {
+            loading: false,
+            error: false,
+        },
         getForecast: function () {
             var self = this;
             var apiUrl = "/api/weather/current?latitude=41.8854710&longitude=-87.6430260";
+            self.loading = true;
             $http.get( apiUrl ).
                 success( function ( data, status ) {
                     self.forecast = data;
+                    self.status.loading = false;
+                    self.status.error = false;
                 } ).
                 error( function ( data, status ) {
                     logError( data );
+                    self.status.loading = false;
+                    self.status.error = true;
                 } );
         }
     };
