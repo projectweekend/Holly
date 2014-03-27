4e3ad971930b3d2ff5d00af26e870d42556fce0b
Brian Hines <brian@projectweekend.net>
2014-03-27 06:36:12 -0500

    Make service return a function so separate objects can be created using new

--- webapp/public/js/services_weather.js
+++ webapp/public/js/services_weather.js
@@ -12,28 +12,32 @@ var svcMod = angular.module('myApp.services_weather', []);
 
 svcMod.factory( "WeatherIO", function ( $http ) {
 
-    return {
-        forecast: {},
-        status: {
-            loading: false,
-            error: false,
-        },
-        getForecast: function ( forecastType ) {
-            var self = this;
-            var apiUrl = "/api/weather/" + forecastType + "?latitude=41.8854710&longitude=-87.6430260";
-            self.loading = true;
-            $http.get( apiUrl ).
-                success( function ( data, status ) {
-                    self.forecast = data;
-                    self.status.loading = false;
-                    self.status.error = false;
-                } ).
-                error( function ( data, status ) {
-                    logError( data );
-                    self.status.loading = false;
-                    self.status.error = true;
-                } );
-        }
+    return function () {
+
+        return {
+            forecast: {},
+            status: {
+                loading: false,
+                error: false,
+            },
+            getForecast: function ( forecastType ) {
+                var self = this;
+                var apiUrl = "/api/weather/" + forecastType + "?latitude=41.8854710&longitude=-87.6430260";
+                self.loading = true;
+                $http.get( apiUrl ).
+                    success( function ( data, status ) {
+                        self.forecast = data;
+                        self.status.loading = false;
+                        self.status.error = false;
+                    } ).
+                    error( function ( data, status ) {
+                        logError( data );
+                        self.status.loading = false;
+                        self.status.error = true;
+                    } );
+            }
+        };
+
     };
 
 } );
