f1751575d74d2e7c2f931aac6110955530e8434c
Brian Hines <brian@projectweekend.net>
2014-06-26 05:57:05 -0500

    Will revisit socket stuff later too

--- webapp/routes/socket.js
+++ webapp/routes/socket.js
@@ -12,40 +12,4 @@ module.exports = function ( socket ) {
 
 	var handleError = ERRORSocket( socket, {route: 'app:error'} );
 
-	// The latest System Temperature reading is broadcast on a set interval
-	RepeatOneSocket( socket, handleError, {
-		route: 'updates:system:temp',
-		model: SystemTemperatureData,
-		query: {},
-		sort: '-date',
-		interval: 120000
-	} );
-
-	// The latest Starbug Temperature reading is broadcast on a set interval
-	RepeatOneSocket( socket, handleError, {
-		route: 'updates:starbug:temp',
-		model: SystemTemperatureData,
-		query: {},
-		sort: '-date',
-		interval: 120000
-	} );
-
-	// The latest Indoor Temperature reading is broadcast on a set interval
-	RepeatOneSocket( socket, handleError, {
-		route: 'updates:indoor:temp',
-		model: IndoorTemperatureData,
-		query: {},
-		sort: '-date',
-		interval: 120000
-	} );
-
-	// The latest Indoor Humidity reading is broadcast on a set interval
-	RepeatOneSocket( socket, handleError, {
-		route: 'updates:indoor:humidity',
-		model: IndoorHumidityData,
-		query: {},
-		sort: '-date',
-		interval: 120000
-	} );
-
 };
