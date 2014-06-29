6ddb4fc27c4c9a9e329e7a145794d5f47e7aed21
Brian Hines <brian@projectweekend.net>
2014-06-29 08:28:20 -0500

    Clean up angular app module

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,23 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var aMod = angular.module( 'myApp', [
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers'
+] );
+
+aMod.config( [ '$routeProvider', function( $routeProvider ) {
+
+    $routeProvider.when( '/charts', {
+        templateUrl: 'partials/charts.html',
+        controller: 'Charts'
+    } );
+
+    $routeProvider.otherwise( {
+        redirectTo: '/charts'
+    } );
+
+} ] );
