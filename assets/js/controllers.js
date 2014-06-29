66a0cf0bac9bd395aac5aa8a45b2280ca6cf188b
Brian Hines <brian@projectweekend.net>
2014-06-29 09:49:50 -0500

    Adding controllers

---
+++ assets/js/controllers.js
@@ -0,0 +1,16 @@
+var cMod = angular.module( 'myApp.controllers', [] );
+
+
+cMod.controller( 'Charts', [ '$scope', function( $scope ) {
+
+} ] );
+
+
+cMod.controller( 'Transit', [ '$scope', function( $scope ) {
+
+} ] );
+
+
+cMod.controller( 'Weather', [ '$scope', function( $scope ) {
+
+} ] );
