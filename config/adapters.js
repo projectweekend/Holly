7b19347e78876253f02333874a0acbc8e53d0611
Brian Hines <brian@projectweekend.net>
2014-06-29 10:31:38 -0500

    Get mongo running in fig

--- config/adapters.js
+++ config/adapters.js
@@ -2,7 +2,7 @@ module.exports.adapters = {
     'default': 'mongo',
     mongo: {
         module: 'sails-mongo',
-        host: process.env.MONGO_HOST,
+        host: process.env.DB_1_PORT_27017_TCP_ADDR || process.env.MONGO_HOST,
         port: 27017,
         database: process.env.MONGO_DATABASE
     }
