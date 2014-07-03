58a3a6bf9b30e05ad9b19697e9b459412fe81e59
Brian Hines <brian@projectweekend.net>
2014-07-03 14:08:56 -0500

    Install Jade for home page

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var routes = require( './routes/index' );
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded() );
