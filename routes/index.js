65517d2c00da06d54f98640a3ea9df6aa1c49bb1
Brian Hines <brian@projectweekend.net>
2014-07-03 14:15:42 -0500

    Adding index page for Angular app

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,16 @@ var router = express.Router();
 var SensorAPI = require( '../api/sensor/controllers' );
 
 
+// Serve the index page for the Angular app
+router.get( '/', function ( req, res ) {
+
+    res.render( 'index' );
+
+} );
+
+
 /* Map URLs to handlers in this file */
 router.post( '/api/sensor', SensorAPI.create );
-
 router.get( '/api/chart/:chartType', SensorAPI.getChart );
 
 
