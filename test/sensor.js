f2ccadd7d4ec37e553ea1a38145d6941d7e4e3ea
Brian Hines <brian@projectweekend.net>
2014-07-02 21:46:50 -0500

    Finish tests for sensor route

--- test/sensor.js
+++ test/sensor.js
@@ -6,7 +6,6 @@ var api = supertest( 'http://192.168.13.81:3000' );
 var authToken =  "Bearer " + process.env.AUTH_TOKEN;
 
 
-
 describe( 'add a sensor reading...', function () {
 
     describe( 'with valid data...', function () {
@@ -40,6 +39,7 @@ describe( 'add a sensor reading...', function () {
                     done();
 
                 } );
+
         } );
 
     } );
@@ -47,7 +47,34 @@ describe( 'add a sensor reading...', function () {
     describe( 'with invalid temp_c', function () {
 
         it( 'responds with 400 and a message', function ( done ) {
-            done();
+
+            api.post( '/api/sensor' )
+                .set( 'Content-Type', 'application/json' )
+                .set( 'Authorization', authToken )
+                .send( {
+                    temp_c: "not valid",
+                    temp_f: 71.6,
+                    humidity: 55.3,
+                    pressure: 876.3,
+                    luminosity: 79.2
+                } )
+                .expect( 400 )
+                .end( function ( err, res ) {
+
+                    if ( err ) {
+                        return done( err );
+                    }
+
+                    expect( res.body ).to.include( {
+                        "param": "temp_c",
+                        "msg": "'temp_c' must be a float",
+                        "value": "not valid"
+                    } );
+
+                    done();
+
+                } );
+
         } );
 
     } );
@@ -55,7 +82,34 @@ describe( 'add a sensor reading...', function () {
     describe( 'with invalid temp_f', function () {
 
         it( 'responds with 400 and a message', function ( done ) {
-            done();
+
+            api.post( '/api/sensor' )
+                .set( 'Content-Type', 'application/json' )
+                .set( 'Authorization', authToken )
+                .send( {
+                    temp_c: 22.0,
+                    temp_f: "not valid",
+                    humidity: 55.3,
+                    pressure: 876.3,
+                    luminosity: 79.2
+                } )
+                .expect( 400 )
+                .end( function ( err, res ) {
+
+                    if ( err ) {
+                        return done( err );
+                    }
+
+                    expect( res.body ).to.include( {
+                        "param": "temp_f",
+                        "msg": "'temp_f' must be a float",
+                        "value": "not valid"
+                    } );
+
+                    done();
+
+                } );
+
         } );
 
     } );
@@ -63,7 +117,34 @@ describe( 'add a sensor reading...', function () {
     describe( 'with invalid humidity', function () {
 
         it( 'responds with 400 and a message', function ( done ) {
-            done();
+
+            api.post( '/api/sensor' )
+                .set( 'Content-Type', 'application/json' )
+                .set( 'Authorization', authToken )
+                .send( {
+                    temp_c: 22.0,
+                    temp_f: 71.6,
+                    humidity: "not valid",
+                    pressure: 876.3,
+                    luminosity: 79.2
+                } )
+                .expect( 400 )
+                .end( function ( err, res ) {
+
+                    if ( err ) {
+                        return done( err );
+                    }
+
+                    expect( res.body ).to.include( {
+                        "param": "humidity",
+                        "msg": "'humidity' must be a float",
+                        "value": "not valid"
+                    } );
+
+                    done();
+
+                } );
+
         } );
 
     } );
@@ -71,7 +152,34 @@ describe( 'add a sensor reading...', function () {
     describe( 'with invalid pressure', function () {
 
         it( 'responds with 400 and a message', function ( done ) {
-            done();
+
+            api.post( '/api/sensor' )
+                .set( 'Content-Type', 'application/json' )
+                .set( 'Authorization', authToken )
+                .send( {
+                    temp_c: 22.0,
+                    temp_f: 71.6,
+                    humidity: 55.3,
+                    pressure: "not valid",
+                    luminosity: 79.2
+                } )
+                .expect( 400 )
+                .end( function ( err, res ) {
+
+                    if ( err ) {
+                        return done( err );
+                    }
+
+                    expect( res.body ).to.include( {
+                        "param": "pressure",
+                        "msg": "'pressure' must be a float",
+                        "value": "not valid"
+                    } );
+
+                    done();
+
+                } );
+
         } );
 
     } );
@@ -79,7 +187,34 @@ describe( 'add a sensor reading...', function () {
     describe( 'with invalid luminosity', function () {
 
         it( 'responds with 400 and a message', function ( done ) {
-            done();
+
+            api.post( '/api/sensor' )
+                .set( 'Content-Type', 'application/json' )
+                .set( 'Authorization', authToken )
+                .send( {
+                    temp_c: 22.0,
+                    temp_f: 71.6,
+                    humidity: 55.3,
+                    pressure: 876.3,
+                    luminosity: "not valid"
+                } )
+                .expect( 400 )
+                .end( function ( err, res ) {
+
+                    if ( err ) {
+                        return done( err );
+                    }
+
+                    expect( res.body ).to.include( {
+                        "param": "luminosity",
+                        "msg": "'luminosity' must be a float",
+                        "value": "not valid"
+                    } );
+
+                    done();
+
+                } );
+
         } );
 
     } );
