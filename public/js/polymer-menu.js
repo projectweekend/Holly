a073cfb1af7ab016d1f0349b1eb2ab8873ad078a
Brian Hines <brian@projectweekend.net>
2014-07-04 08:24:52 -0500

    Getting a few things straight

---
+++ public/js/polymer-menu.js
@@ -0,0 +1,9 @@
+document.addEventListener( 'polymer-ready', function() {
+
+    var navicon = document.getElementById( 'navicon' );
+    var drawerPanel = document.getElementById( 'drawerPanel' );
+    navicon.addEventListener( 'click', function() {
+        drawerPanel.togglePanel();
+    } );
+
+} );
