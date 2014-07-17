f1ab84f1095610b203900fb02cad5cd5ee8af4b9
Brian Hines <brian@projectweekend.net>
2014-07-17 05:58:50 -0500

    Add Raspberry Pi chart service

--- public/js/services.js
+++ public/js/services.js
@@ -167,6 +167,34 @@ sMod.factory( 'SensorChart', function ( API, $window ) {
 
 
 sMod.factory( 'RaspberryPiChart', function ( API, $window ) {
-    return {
+
+    var chartWidth = function () {
+
+        return ( $window.innerWidth - 150 );
+
     };
+
+    return {
+        width: chartWidth(),
+        data:{
+            labels: [],
+            datasets: []
+        },
+        init: function () {
+
+            var self = this;
+
+            API.get( '/api/chart/raspberry-pi', function ( err, data ) {
+
+                if ( err ) {
+                    return alert( "Error with raspberry-pi chart" );
+                }
+
+                self.data.labels = makeLabels( data );
+                self.data.datasets = makeDataSets( data, 'temp_f' );
+
+            } );
+        }
+    };
+
 } );
