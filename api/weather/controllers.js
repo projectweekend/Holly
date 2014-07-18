2c1fe2cb44164eb95d6686e0ad3cdfbdfa7ea7f7
Brian Hines <brian@projectweekend.net>
2014-07-17 20:25:12 -0500

    Add controller for weather API

---
+++ api/weather/controllers.js
@@ -0,0 +1,32 @@
+var forecastIO = require( 'weatherman.io' );
+var systemError = require( '../utils' ).systemError;
+var handleRouteError = require( '../utils' ).handleRouteError;
+
+
+
+var apiKey = process.env.FORECAST_IO_KEY;
+var latitude = process.env.FORECAST_LATITUDE;
+var longitude = process.env.FORECAST_LONGITUDE;
+var weatherman = forecastIO( apiKey );
+
+
+weatherman.goOnLocation( latitude, longitude );
+weatherman.options = {
+    exclude: [ "minutely", "hourly", "daily", "alerts" ]
+};
+
+
+exports.current = function ( req, res ) {
+
+    weatherman.doForecast( function ( err, weatherReport ) {
+
+        if ( err ) {
+            var error = systemError( "Forecast.io API Error" );
+            return handleRouteError( error, res );
+        }
+
+        return res.json( weatherReport, 200 );
+
+    } );
+
+};
