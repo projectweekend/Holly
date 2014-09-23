709582e295586887cd7025270cfe7671ed00aad5
Brian Hines <brian@projectweekend.net>
2014-09-23 09:47:18 -0500

    Working script that populates averages

--- api/sensor/jobs/CalculateWeeklyAverage.js
+++ api/sensor/jobs/CalculateWeeklyAverage.js
@@ -21,31 +21,82 @@ var momentWithNoTime = function () {
 };
 
 
-var weekStart = momentWithNoTime().subtract(7, "days").toDate();
-var weekEnd = momentWithNoTime().subtract(1, "days").toDate();
+var weekStart = momentWithNoTime().subtract( 7, "days" ).toDate();
+var weekEnd = momentWithNoTime().toDate();
+var calcDate = momentWithNoTime().subtract( 1, "days" ).toDate();
 
-console.log( weekStart );
-console.log( weekEnd );
+
+var logError = function ( err ) {
+    console.log( err );
+    process.exit( 1 );
+};
+
+
+var logNoData = function () {
+    console.log( "No sensors readings between " + weekStart.toDateString() + " & " + weekEnd.toDateString() );
+    process.exit( 1 );
+};
+
+
+var saveAverageData = function ( calcData ) {
+
+    delete calcData._id;
+    calcData.date = calcDate;
+    calcData.type = "WEEKLY";
+
+    AverageSensorReading.create( calcData, function ( err, avgReading ) {
+
+        if ( err ) {
+            console.log( err );
+            process.exit( 1 );
+        }
+
+        process.exit( 0 );
+
+    } );
+
+};
+
 
 SensorReading.aggregate( [
     {
+        $match: {
+            date: {
+                $lt: weekEnd,
+                $gte: weekStart
+            }
+        }
+    },
+    {
         $group: {
             _id: null,
-            avgTempC: {
+            temp_c: {
                 $avg: "$temp_c"
             },
-            avgTempF: {
+            temp_f: {
                 $avg: "$temp_f"
+            },
+            humidity: {
+                $avg: "$humidity"
+            },
+            pressure: {
+                $avg: "$pressure"
+            },
+            luminosity: {
+                $avg: "$luminosity"
             }
         }
     }
 ], function ( err, data ) {
+
     if ( err ) {
-        console.log( err );
-        process.exit( 1 );
+        logError();
     }
-    console.log( data );
-    process.exit( 0 );
+
+    if ( data.length === 0 ) {
+        logNoData();
+    }
+
+    saveAverageData( data[ 0 ] );
+
 } );
-
-
