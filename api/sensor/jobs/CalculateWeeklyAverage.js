32e9b77b32d5970361313734ac3c8661d43c5432
Brian Hines <brian@projectweekend.net>
2014-09-24 13:45:47 -0500

    Update weekly average script

--- api/sensor/jobs/CalculateWeeklyAverage.js
+++ api/sensor/jobs/CalculateWeeklyAverage.js
@@ -13,36 +13,7 @@ var weekEnd = jobUtils.momentDateOnly().toDate();
 var calcDate = jobUtils.momentDateOnly().subtract( 1, "days" ).toDate();
 
 
-SensorReading.aggregate( [
-    {
-        $match: {
-            date: {
-                $lt: weekEnd,
-                $gte: weekStart
-            }
-        }
-    },
-    {
-        $group: {
-            _id: null,
-            temp_c: {
-                $avg: "$temp_c"
-            },
-            temp_f: {
-                $avg: "$temp_f"
-            },
-            humidity: {
-                $avg: "$humidity"
-            },
-            pressure: {
-                $avg: "$pressure"
-            },
-            luminosity: {
-                $avg: "$luminosity"
-            }
-        }
-    }
-], function ( err, data ) {
+SensorReading.averageOverDateRange( weekStart, weekEnd, function ( err, data ) {
 
     if ( err ) {
         jobUtils.logError();
