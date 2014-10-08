a973af5e2464c3c784ffdbef4e5866d9ee7f04d2
Brian Hines <brian@projectweekend.net>
2014-10-08 06:11:59 -0500

    Monthly and Yearly charts

--- public/build/project.js
+++ public/build/project.js
@@ -134,6 +134,38 @@ cMod.controller( 'RaspberryPi', function ( $scope, RaspberryPiChart ) {
 var cMod = angular.module( 'myApp.controller-temperature-average', [] );
 
 
+var makeDataSets = function ( rawChartData ) {
+
+    var extractPropertyData = function ( chartItem ) {
+        return chartItem[ "avg_temp_f" ];
+    };
+
+    return {
+        fillColor : "rgba(230,126,34,0.5)",
+        strokeColor : "rgba(230,126,34,1)",
+        highlightFill: "rgba(230,126,34,0.8)",
+        highlightStroke: "rgba(230,126,34,1)",
+        data: rawChartData.map( extractPropertyData )
+    };
+
+};
+
+
+var makeLabels = function ( rawChartData ) {
+
+    var extractDateParts = function ( chartItem ) {
+        var d = new Date( chartItem.date );
+        var day = d.getDate();
+        var month = d.getMonth() + 1;
+        var year = d.getFullYear();
+        return year + "/" + month + "/" + day;
+    };
+
+    return rawChartData.map( extractDateParts );
+
+};
+
+
 cMod.controller( 'TemperatureAverageWeekly',
     function( $scope, ActiveMenuItem, API ) {
 
@@ -146,59 +178,59 @@ cMod.controller( 'TemperatureAverageWeekly',
 
         var apiURL = "/api/chart/temperature/stats?stat=WEEKLY&readings=52";
 
-        var makeLabels = function ( rawChartData ) {
-
-            var extractDateParts = function ( chartItem ) {
-                var d = new Date( chartItem.date );
-                var day = d.getDate();
-                var month = d.getMonth() + 1;
-                var year = d.getFullYear();
-                return year + "/" + month + "/" + day;
-            };
-
-            return rawChartData.map( extractDateParts );
-
-        };
-
-        var makeDataSets = function ( rawChartData, propertyName ) {
-
-            var extractPropertyData = function ( chartItem ) {
-                return chartItem[ propertyName ];
-            };
-
-            return {
-                fillColor : "rgba(230,126,34,0.5)",
-                strokeColor : "rgba(230,126,34,1)",
-                highlightFill: "rgba(230,126,34,0.8)",
-                highlightStroke: "rgba(230,126,34,1)",
-                data: rawChartData.map( extractPropertyData )
-            };
-
-        };
-
         API.get( apiURL, function ( err, data ) {
             if ( err ) {
-                return alert( "Error with weekly average temperature chart" );
+                return alert( "Error with average temperature chart" );
             }
             $scope.chart.labels = makeLabels( data );
-            $scope.chart.datasets.push( makeDataSets( data, "avg_temp_f" ) );
+            $scope.chart.datasets.push( makeDataSets( data ) );
         } );
 
     } );
 
 
 cMod.controller( 'TemperatureAverageMonthly',
-    function( $scope, ActiveMenuItem ) {
+    function( $scope, ActiveMenuItem, API ) {
 
         $scope.ActiveMenuItem = ActiveMenuItem;
+
+        $scope.chart = {
+            labels: [],
+            datasets: []
+        };
+
+        var apiURL = "/api/chart/temperature/stats?stat=MONTHLY&readings=12";
+
+        API.get( apiURL, function ( err, data ) {
+            if ( err ) {
+                return alert( "Error with average temperature chart" );
+            }
+            $scope.chart.labels = makeLabels( data );
+            $scope.chart.datasets.push( makeDataSets( data ) );
+        } );
 
     } );
 
 
 cMod.controller( 'TemperatureAverageYearly',
-    function( $scope, ActiveMenuItem ) {
+    function( $scope, ActiveMenuItem, API ) {
 
         $scope.ActiveMenuItem = ActiveMenuItem;
+
+        $scope.chart = {
+            labels: [],
+            datasets: []
+        };
+
+        var apiURL = "/api/chart/temperature/stats?stat=YEARLY&readings=5";
+
+        API.get( apiURL, function ( err, data ) {
+            if ( err ) {
+                return alert( "Error with average temperature chart" );
+            }
+            $scope.chart.labels = makeLabels( data );
+            $scope.chart.datasets.push( makeDataSets( data ) );
+        } );
 
     } );
 
