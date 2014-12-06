3a81c4cafa87caa0c431a8ba36a820865c888659
Brian Hines <brian@projectweekend.net>
2014-12-06 11:02:15 -0600

    Organize this file a little differently

--- api/sensor/models.js
+++ api/sensor/models.js
@@ -15,96 +15,90 @@ var SensorReadingSchema = Schema( {
     luminosity: Number
 } );
 
+SensorReadingSchema.statics = {
+    add: function ( data, cb ) {
+        this.create( data, function ( err, reading ) {
+            if ( err ) {
+                return cb( systemError( err ) );
+            }
+            return cb( null, reading );
+        } );
+    },
+    chartReadings: function ( numberOfReadings, cb ) {
+        var q = this.find( {} )
+                    .limit( numberOfReadings )
+                    .sort( '-date' );
+        q.exec( function ( err, readings ) {
+            if ( err ) {
+                return cb( systemError( err ) );
+            }
+            return cb( null, readings );
+        } );
+    },
+    calcStatsOverDateRange: function ( startDate, endDate, cb ) {
+        var matchOptions = {
+            date: {
+                $gte: startDate,
+                $lte: endDate
+            }
+        };
 
-SensorReadingSchema.statics.add = function ( data, cb ) {
-    this.create( data, function ( err, reading ) {
-        if ( err ) {
-            return cb( systemError( err ) );
-        }
-        return cb( null, reading );
-    } );
+        var groupOptions = {
+            _id: null,
+            avg_temp_c: {
+                $avg: "$temp_c"
+            },
+            min_temp_c: {
+                $min: "$temp_c"
+            },
+            max_temp_c: {
+                $max: "$temp_c"
+            },
+            avg_temp_f: {
+                $avg: "$temp_f"
+            },
+            min_temp_f: {
+                $min: "$temp_f"
+            },
+            max_temp_f: {
+                $max: "$temp_f"
+            },
+            avg_humidity: {
+                $avg: "$humidity"
+            },
+            min_humidity: {
+                $min: "$humidity"
+            },
+            max_humidity: {
+                $max: "$humidity"
+            },
+            avg_pressure: {
+                $avg: "$pressure"
+            },
+            min_pressure: {
+                $min: "$pressure"
+            },
+            max_pressure: {
+                $max: "$pressure"
+            },
+            avg_luminosity: {
+                $avg: "$luminosity"
+            },
+            min_luminosity: {
+                $min: "$luminosity"
+            },
+            max_luminosity: {
+                $max: "$luminosity"
+            }
+        };
+
+        var q = this.aggregate()
+                    .match( matchOptions )
+                    .group( groupOptions );
+
+        q.exec( cb );
+    }
 };
-
-
-SensorReadingSchema.statics.chartReadings = function ( numberOfReadings, cb ) {
-    var q = this.find( {} )
-                .limit( numberOfReadings )
-                .sort( '-date' );
-    q.exec( function ( err, readings ) {
-        if ( err ) {
-            return cb( systemError( err ) );
-        }
-        return cb( null, readings );
-    } );
-};
-
-
-SensorReadingSchema.statics.statsOverDateRange = function ( startDate, endDate, cb ) {
-
-    var matchOptions = {
-        date: {
-            $gte: startDate,
-            $lte: endDate
-        }
-    };
-
-    var groupOptions = {
-        _id: null,
-        avg_temp_c: {
-            $avg: "$temp_c"
-        },
-        min_temp_c: {
-            $min: "$temp_c"
-        },
-        max_temp_c: {
-            $max: "$temp_c"
-        },
-        avg_temp_f: {
-            $avg: "$temp_f"
-        },
-        min_temp_f: {
-            $min: "$temp_f"
-        },
-        max_temp_f: {
-            $max: "$temp_f"
-        },
-        avg_humidity: {
-            $avg: "$humidity"
-        },
-        min_humidity: {
-            $min: "$humidity"
-        },
-        max_humidity: {
-            $max: "$humidity"
-        },
-        avg_pressure: {
-            $avg: "$pressure"
-        },
-        min_pressure: {
-            $min: "$pressure"
-        },
-        max_pressure: {
-            $max: "$pressure"
-        },
-        avg_luminosity: {
-            $avg: "$luminosity"
-        },
-        min_luminosity: {
-            $min: "$luminosity"
-        },
-        max_luminosity: {
-            $max: "$luminosity"
-        }
-    };
-
-    var q = this.aggregate()
-                .match( matchOptions )
-                .group( groupOptions );
-
-    q.exec( cb );
-
-};
-
 
 exports.SensorReading = mongoose.model( 'SensorReading', SensorReadingSchema );
 
@@ -130,32 +124,29 @@ var SensorStatsSchema = Schema( {
     max_luminosity: Number
 } );
 
-
 SensorStatsSchema.index( { date: 1, type: 1 }, { unique: true } );
 
-
-SensorStatsSchema.statics.add = function ( data, cb ) {
-    delete data._id;
-    this.create( data, function ( err, reading ) {
-        if ( err ) {
-            return cb( systemError( err ) );
-        }
-        return cb( null, reading );
-    } );
+SensorStatsSchema.statics = {
+    add: function ( data, cb ) {
+        delete data._id;
+        this.create( data, function ( err, reading ) {
+            if ( err ) {
+                return cb( systemError( err ) );
+            }
+            return cb( null, reading );
+        } );
+    },
+    chartReadings: function ( options, cb ) {
+        var q = this.find( { type: options.statType } )
+                    .limit( options.numberOfReadings )
+                    .sort( '-date' );
+        q.exec( function ( err, readings ) {
+            if ( err ) {
+                return cb( systemError( err ) );
+            }
+            return cb( null, readings );
+        } );
+    }
 };
-
-
-SensorStatsSchema.statics.chartReadings = function ( options, cb ) {
-    var q = this.find( { type: options.statType } )
-                .limit( options.numberOfReadings )
-                .sort( '-date' );
-    q.exec( function ( err, readings ) {
-        if ( err ) {
-            return cb( systemError( err ) );
-        }
-        return cb( null, readings );
-    } );
-};
-
 
 exports.SensorStats = mongoose.model( 'SensorStats', SensorStatsSchema );
