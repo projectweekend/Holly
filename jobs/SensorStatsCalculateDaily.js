0c33fdff8aafa88f3b3c125a0c1efa7eb6b5fdca
Brian Hines <brian@projectweekend.net>
2014-12-06 11:16:18 -0600

    Start a daily calc job

---
+++ jobs/SensorStatsCalculateDaily.js
@@ -0,0 +1,9 @@
+var utils = require( "./utils" );
+var models = require( '../api/sensor/models' ).SensorReading;
+
+// models.SensorReading
+// models.SensorStats
+
+
+// connect to the db
+utils.connectToMongo();
